c45b28558ea1acacd67031be02ad64d4e776b325
jeongjiwooon <jjwanm@naver.com>
2023-08-01 17:55:25 +0900

    커밋

--- datatype_strings.py
+++ datatype_strings.py
@@ -1,3 +1,37 @@
-str = "yojulab!"
-len(str)
+# str = "yojulab!"
+# len(str)
+# pass
+
+# len(str)
+# 9
+
+# condition
+# "ju" in str
+# True
+
+# "Not" in str
+# False
+
+# "Not" not in str
+# True
+
+# http://www.w3schools.com/python/python_strings_slicing.asp
+# slicing
+# str[3]
+# pass
+
+# format
+age = 36
+txt = "My name is John, I am " + str(age)
+print(txt)
+txt_second = "My name is John, I am {}."
+txt_second
+'My name is John, I am 36'
+
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {0} pieces of item {1} for {2} dollars. I have just {2}"
 pass
+# myorder.format(quantity, itemno, price)
+# 'I want 3 pieces of item 567 for 49.95 dollars. I have just 49.95'
